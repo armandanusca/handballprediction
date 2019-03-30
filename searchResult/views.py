3944a626e699cb2c356d41533c709821a5d0a200
Vlad Ianculescu <vlad.ianculescu@student.manchester.ac.uk>
2019-03-30 15:49:18 +0000

    Search

--- searchResult/views.py
+++ searchResult/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
+from data import players
+from difflib import SequenceMatcher as match
+import re
+
 
 def searchView(request):
-    return render(request, 'searchResults.html')
+    try:
+        searchString = request.POST['search-input']
+    except:
+        return render(request, 'searchResults.html', {"res":[]})
+
+    res = []
+    print(searchString.split())
+    for i,x in enumerate(players):
+        for name in re.split('\W+',searchString):
+            for xname in re.split('\W+',x['name,position']):
+                if match(None, name.upper(), xname.upper()).ratio() > 0.8:
+                    res.append((x['name,position'],i))
+
+    context = {'res':res}
+
+    return render(request, 'searchResults.html', context)
