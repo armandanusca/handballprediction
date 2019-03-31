26b01c1befe77aa194ed5739999bde82451a0c31
Armand Anusca <anusca.armand@gmail.com>
2019-03-31 10:29:10 +0100

    Fixed stupid data

--- data.py
+++ data.py
@@ -4,29 +4,29 @@ with open('data.txt','r') as f:
 
 players += [{
   "name": "Armand Ioan Anusca Popa",
-  "position": "Center",
+  "position": "Line Player",
   "season": "2015/2016",
-  "predictedGoals": 1223,
-  "actualGoals": 1242
+  "predictedGoals": 3.24,
+  "actualGoals": 3.42
 },
 {
   "name": "Vlad Bucur",
-  "position": "Middle Wing",
+  "position": "Line Player",
   "season": "2017/2018",
-  "predictedGoals": 1421,
-  "actualGoals": 1231
+  "predictedGoals": 4.3,
+  "actualGoals": 3.78
 },
 {
   "name": "Vlad Ianculescu",
-  "position": "Left Center",
-  "season": "2015-2016",
-  "predictedGoals": 1242,
-  "actualGoals": 1243,
+  "position": "Left Back",
+  "season": "2015/2016",
+  "predictedGoals": 2.78,
+  "actualGoals": 1.2,
 },
 {
   "name": "Nora Elena Tuta",
   "position": "Left Wing",
-  "season": "2017-2018",
-  "predictedGoals": 1241,
-  "actualGoals": 1231
+  "season": "2017/2018",
+  "predictedGoals": 8.3,
+  "actualGoals": 7.9
 }]
